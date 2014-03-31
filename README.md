a4fe10e1aecd71676f9ebc2e6a5fa86554954171
Alexandre Rames <alexandre.rames@uop.re>
2014-03-31 12:58:50 +0100

    Introduce an installation script.

--- README.md
+++ README.md
@@ -5,19 +5,8 @@ As usual `:help <topic>` is your friend.
 
 ## Installation
 
-First backup you own configuration files. Then clone this repository and copy
-the configuration files to your runtime path.
-
-    $ git clone git@github.com:arames/vim-config.git && cd vim-config
-    $ cp -r .vimrc .vim ~/
-
-The .vimrc uses the Vundle plugin (https://github.com/gmarik/vundle) to manage
-other plugins. You will need to clone the repo
-
-    $ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-
-and install the plugins by launching `vim` and running `:BundleInstall`.
-
+Installation can be done automatically with the provided script `install.sh`,
+which also serves as documentation.
 
 ### Patches for existing plugins.
 
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+BASE_DIR=~
+TARGET_DIR=$BASE_DIR
+
+# Backup existing configuration.
+BACKUP_DIR=backup.`date +%F-%R`
+mkdir --parents $BACKUP_DIR
+BASE_VIMRC=$BASE_DIR/.vimrc
+BASE_VIM_CONFIG=$BASE_DIR/.vim
+echo "Backing up existing configuration to $BACKUP_DIR"
+if [ -f $BASE_VIMRC ] ; then
+	cp --recursive --no-clobber $BASE_VIMRC $BACKUP_DIR/
+fi
+if [ -f $BASE_VIM_CONFIG ] ; then
+	cp --recursive --no-clobber $BASE_VIM_CONFIG $BACKUP_DIR/
+fi
+
+
+
+echo "Copying new files."
+# Copy the new files.
+cp --recursive .vim $BASE_DIR/
+cp --recursive .vimrc $BASE_DIR/
+echo "Installing the plugin manager."
+git clone https://github.com/gmarik/vundle.git $TARGET_DIR/.vim/bundle/vundle
+echo "Installing plugins."
+vim -c BundleInstall -c qall
+
+
+
+echo "Done."
